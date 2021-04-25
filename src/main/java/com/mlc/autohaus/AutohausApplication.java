31e0ce9cb74b51e3805553fcbfb21970342889c0
Marcelo Lecar <marcelo.lecar@gmail.com>
2021-04-25 14:54:14 +0200

    first commit, api first

---
+++ src/main/java/com/mlc/autohaus/AutohausApplication.java
@@ -0,0 +1,11 @@
+package com.mlc.autohaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutohausApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AutohausApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mlc/autohaus/AutohausApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlc.autohaus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutohausApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
